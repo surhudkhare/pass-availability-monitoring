f3d69f65192cf3613fc9b6f307da587a11b54869
Surhud <surhudk@gmail.com>
2024-07-26 11:56:58 -0700

    Display status with timestamp

--- passavailabilitymonitor/main.py
+++ passavailabilitymonitor/main.py
@@ -4,10 +4,12 @@ from datetime import timedelta
 from passavailabilitymonitor import config
 from passavailabilitymonitor.monitoring import check_availability
 from passavailabilitymonitor.notification import notify
+from passavailabilitymonitor.utils import get_time
 
 def define_availability_job(trail, target_date, mode="test"):
-    print(f"Monitoring {trail} for {target_date} in {mode} mode")
-    if check_availability(trail=trail, target_date=target_date, mode=mode):
+    availability = check_availability(trail=trail, target_date=target_date, mode=mode)
+    print(f"{get_time()} - {trail} | {target_date} | {availability}")
+    if availability:
         notify()
 
 if __name__ == "__main__":
@@ -18,7 +20,7 @@ if __name__ == "__main__":
             schedule.every(10).seconds.until(timedelta(minutes=10)).do(define_availability_job, trail="Test trail", target_date="Test date", mode="test")
     elif mode == "L":        
         trail_name = input("Please enter trail name: ").lower()
-        target_date = input("Please enter target date in the format YYYY-MM-DD: ")
+        target_date = input("Please enter target date in the format YYYY-MM-DD: ") # TODO: Check Today <= Date <= Today+2
         schedule.every(1).minutes.until(timedelta(hours=3)).do(define_availability_job, trail=trail_name, target_date=target_date, mode="live")
     while True:
         # print(f"Start monitoring {trail_name} for {target_date} - {datetime.today().strftime("%d-%b-%Y %H:%M:%S")}")
