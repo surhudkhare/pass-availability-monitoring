e4e7a3d6fe96bf1789ffdd3a0d26486f6ebd4b4b
Surhud <surhudk@gmail.com>
2024-07-26 08:49:58 -0700

    Move scheduling to main.py

--- passavailabilitymonitor/jobschedule.py
+++
@@ -1,7 +0,0 @@
-from passavailabilitymonitor.monitoring import check_availability
-from passavailabilitymonitor.notification import notify
-
-def define_availability_job():
-    if check_availability(trail="CHEAKAMUS", target_date="2024-07-25"):
-        notify()
-
