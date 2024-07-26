fe34d4ccfa40197f9a2f7878f7c269499382bf81
Surhud <surhudk@gmail.com>
2024-07-26 08:40:53 -0700

    Add get_time

--- passavailabilitymonitor/utils.py
+++ passavailabilitymonitor/utils.py
@@ -1,5 +1,17 @@
 import time
+from datetime import datetime, timedelta
 
 def get_date_string(date):
     date_object = time.strptime(date, "%Y-%m-%d")
     return time.strftime("%A, %B %d, %Y", date_object)
+
+def get_time_elapsed(start_time):
+    
+    return datetime.today()
+
+def get_time():
+    return datetime.today().strftime("%H:%M:%S %d-%b-%Y")
+
+if __name__ == "__main__":
+    t = get_time()
+    print(t)
