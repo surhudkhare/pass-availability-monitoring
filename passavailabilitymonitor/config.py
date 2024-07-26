c205cff713ccc11497bd3c820f91d5cfdc50fd59
Surhud <surhudk@gmail.com>
2024-07-26 11:28:04 -0700

    Change trails keys to lower case

--- passavailabilitymonitor/config.py
+++ passavailabilitymonitor/config.py
@@ -1,8 +1,8 @@
 BCPARKS_URL = "https://reserve.bcparks.ca/dayuse/"
 TRAILS = {
-    'DIAMOND HEAD': {'PARK_NAME': 'Garibaldi', 'TRAIL_NAME': 'Diamond'},
-    'JOFFRE LAKE': {'PARK_NAME': 'Joffre', 'TRAIL_NAME': 'Joffre'},
-    'RUBBLE CREEK': {'PARK_NAME': 'Garibaldi', 'TRAIL_NAME': 'Rubble'},
-    'CHEAKAMUS': {'PARK_NAME': 'Garibaldi', 'TRAIL_NAME': 'Cheakamus'}
+    'diamond head': {'PARK_NAME': 'Garibaldi', 'TRAIL_NAME': 'Diamond'},
+    'joffre': {'PARK_NAME': 'Joffre', 'TRAIL_NAME': 'Joffre'},
+    'rubble creek': {'PARK_NAME': 'Garibaldi', 'TRAIL_NAME': 'Rubble'},
+    'cheakamus': {'PARK_NAME': 'Garibaldi', 'TRAIL_NAME': 'Cheakamus'}
     }
 SCHEDULE_INTERVAL_MINUTES = 2
