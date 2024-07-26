2b524ab9819501f86bb87115e26c63249273d44c
Surhud <surhudk@gmail.com>
2024-07-26 08:41:03 -0700

    Add desktop notification

--- passavailabilitymonitor/notification.py
+++ passavailabilitymonitor/notification.py
@@ -1,6 +1,8 @@
 import smtplib
 from email.mime.text import MIMEText
 import os
+import subprocess
+from passavailabilitymonitor.utils import get_time
 
 def send_email_notifications(to_email, subject, message):
     from_email = os.getenv('SENDER_USERNAME')
@@ -15,7 +17,19 @@ def send_email_notifications(to_email, subject, message):
         server.login(from_email, password)
         server.sendmail(from_email, to_email, msg.as_string())
 
-def notify():
+def send_desktop_notification(title, message):
+    script = f'display notification "{message}" with title "{title}"'
+    subprocess.run(['osascript', '-e', script])
+
+
+def notify(mode="desktop"):
     subject = 'GO GO GO'
-    message = 'The passes are now available! Check the website.'
-    send_email_notifications(os.getenv('TO_EMAIL'), subject, message)
+    message = f'{get_time()}\nThe passes are now available! Check the website'
+    if mode == 'email':
+        send_email_notifications(os.getenv('TO_EMAIL'), subject, message)
+    else:
+        send_desktop_notification(subject, message)
+
+
+if __name__ == "__main__":
+    notify()
